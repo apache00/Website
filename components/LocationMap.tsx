934eb82b36c6309bf178909099cb745eb32f94f5
Gaurav Gupta <gagu12ec@gmail.com>
2025-08-26 19:57:31 +1000

    build fix

--- components/LocationMap.tsx
+++ components/LocationMap.tsx
@@ -1,18 +1,18 @@
 
-"use client";
+// "use client";
 
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import L from "leaflet";
+// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+// import L from "leaflet";
 
 // ✅ Fix default Leaflet marker icons
-const DefaultIcon = new L.Icon({
-    iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-    iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
-    shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-});
-L.Marker.prototype.options.icon = DefaultIcon;
+// const DefaultIcon = new L.Icon({
+//     iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+//     iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+//     shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+//     iconSize: [25, 41],
+//     iconAnchor: [12, 41],
+// });
+// L.Marker.prototype.options.icon = DefaultIcon;
 
 type SimpleMapProps = {
     lat: number;
@@ -23,7 +23,7 @@ type SimpleMapProps = {
 export default function LocationMap({ lat, lng, label = "Location" }: SimpleMapProps) {
     return (
     <div className="w-full h-[300px] sm:h-[400px] md:h-[500px] rounded-lg overflow-hidden shadow">
-        <MapContainer
+        {/* <MapContainer
         center={[lat, lng]}
         zoom={15}
         scrollWheelZoom={false}
@@ -36,7 +36,7 @@ export default function LocationMap({ lat, lng, label = "Location" }: SimpleMapP
         <Marker position={[lat, lng]}>
             <Popup>{label}</Popup>
         </Marker>
-        </MapContainer>
+        </MapContainer> */}
     </div>
     );
 }
